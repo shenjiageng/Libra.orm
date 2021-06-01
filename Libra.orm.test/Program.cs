f17fc98f0e240ebd09d3f2870b7412625e5a66e3
shenjiageng <321272356@qq.com>
2021-06-01 14:14:49 +0800

    处理差异

--- Libra.orm.test/Program.cs
+++ Libra.orm.test/Program.cs
@@ -15,7 +15,7 @@ namespace Libra.orm.test
     {
         static void Main(string[] args)
         {
-            string connectionString = "Data Source=192.168.66.18;Initial Catalog=hwj_test;User ID=sa;Password=sql@123;Min Pool Size=10;";
+            string connectionString = "Data Source=**;Initial Catalog=**;User ID=**;Password=**;";
 
             //LibraContent content = new LibraContent(new LibraConfigure
             //{
